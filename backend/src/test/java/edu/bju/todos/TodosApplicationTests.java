53cb4790461b513f4e86351bd75b2afb883b6860
Ethan McGee <ethanmcgee@iagbenefits.com>
2022-08-28 22:48:52 -0400

    initial commits for code components

---
+++ backend/src/test/java/edu/bju/todos/TodosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bju.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
